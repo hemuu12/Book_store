94ec140d67e6956d0ef548f8f1b2a8c920f2fa12
harisinghbisht <h.bisht@satneel.com>
2025-05-04 12:16:00 +0530

    test

--- src/routes/allroutes.jsx
+++ src/routes/allroutes.jsx
@@ -1,21 +1,34 @@
-import React from 'react'
-import { Route, Routes } from 'react-router-dom'
-import TodoList from '../components/todoList'
-import RegisterForm from '../auth/signup'
-import Login from '../auth/login'
-import ProtectedRoute from '../components/privateRoute'
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import TodoList from '../components/todoList';
+import RegisterForm from '../auth/signup';
+import Login from '../auth/login';
+import ProtectedRoute from '../components/privateRoute';
 
 const Allroutes = () => {
   return (
-    <>
-        <Routes>
-            <Route path="/dashboard" element={<ProtectedRoute><TodoList /></ProtectedRoute>}></Route>
-            <Route path="/signup" element={<RegisterForm />}></Route>
-            <Route path="/login" element={<Login />}></Route>
-        </Routes>
-    
-    </>
-  )
-}
+    <Routes>
+      {/* Root path redirects to dashboard if authenticated */}
+      <Route
+        path="/"
+        element={
+          <ProtectedRoute>
+            <TodoList />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="/dashboard"
+        element={
+          <ProtectedRoute>
+            <TodoList />
+          </ProtectedRoute>
+        }
+      />
+      <Route path="/signup" element={<RegisterForm />} />
+      <Route path="/login" element={<Login />} />
+    </Routes>
+  );
+};
 
-export default Allroutes
+export default Allroutes;
